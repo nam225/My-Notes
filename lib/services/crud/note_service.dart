a2dba03ce38a313fa6c2ed516376357475c85be2
Dao Cu Nam <daocunam225@gmail.com>
2024-04-22 16:50:55 +0700

    step-15

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -12,11 +12,16 @@ class NotesService {
   List<DatabaseNote> _notes = [];
 
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance(){
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      },
+    );
+  }
   factory NotesService() => _shared;
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
@@ -222,6 +227,7 @@ class NotesService {
     try{
       await open();
     } on DatabaseAlreaddyOpenException {
+      print("hi");
     }
   }
   Future<void> open() async {
