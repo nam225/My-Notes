e585732d8d58f041ad1d91957f49934e96945939
Dao Cu Nam <daocunam225@gmail.com>
2024-04-26 22:01:42 +0700

    step-20

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -1,19 +1,19 @@
-class DatabaseAlreaddyOpenException implements Exception {}
+// class DatabaseAlreaddyOpenException implements Exception {}
 
-class UnableToGetDocumentsDirectory implements Exception {}
+// class UnableToGetDocumentsDirectory implements Exception {}
 
-class DatabaseIsNotOpen implements Exception {}
+// class DatabaseIsNotOpen implements Exception {}
 
-class CouldNotDeleteUser implements Exception {}
+// class CouldNotDeleteUser implements Exception {}
 
-class UserAlreadyExists implements Exception {}
+// class UserAlreadyExists implements Exception {}
 
-class CouldNotFindUser implements Exception {}
+// class CouldNotFindUser implements Exception {}
 
-class CouldNotDeleteNote implements Exception {}
+// class CouldNotDeleteNote implements Exception {}
 
-class CouldNotFindNote implements Exception {}
+// class CouldNotFindNote implements Exception {}
 
-class CouldNotUpdateNote implements Exception {}
+// class CouldNotUpdateNote implements Exception {}
 
-class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
+// class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
